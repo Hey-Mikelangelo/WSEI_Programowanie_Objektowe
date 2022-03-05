470114ed990e65967bd5423df99c1800ec5283d1
MichalSipkovski <michal.sipkovski@microsoft.wsei.edu.pl>
2022-03-05 21:13:06 +0100

    Added Fraction class

---
+++ lab_1/lab_1/Fraction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace lab_1
+{
+    public readonly struct Fraction : IEquatable<Fraction>
+    {
+        private readonly int denominator;
+        private readonly int numerator;
+
+        public float Denominator => denominator;
+        public float Numerator => numerator;
+
+        public Fraction(int denominator, int numerator)
+        {
+            int greatestCommonDivisor = MathHelpers.GetGreatestCommonDivisor(denominator, numerator);
+            this.denominator = denominator / greatestCommonDivisor;
+            this.numerator  = numerator / greatestCommonDivisor;
+        }
+
+        public Fraction(Fraction other)
+        {
+            this.denominator = other.denominator;
+            this.numerator = other.numerator;
+        }
+
+        public bool Equals(Fraction other)
+        {
+            return other.numerator == this.numerator && other.denominator == this.denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
