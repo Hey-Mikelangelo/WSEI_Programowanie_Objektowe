306c61da0b668a1bf44402540c633bf59aefc552
MichalSipkovski <michal.sipkovski@microsoft.wsei.edu.pl>
2022-03-05 21:12:17 +0100

    Created method to find greatest common divisor

---
+++ lab_1/lab_1/MathHelpers.cs
@@ -0,0 +1,28 @@
+﻿namespace lab_1
+{
+    public static class MathHelpers
+    {
+        public static int GetGreatestCommonDivisor(int a, int b)
+        {
+            if (a < b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            int a1 = a, b1 = b;
+            do
+            {
+                int a1Prev = a1;
+                a1 = b1;
+                b1 = a1Prev % b1;
+            }
+            while (b1 != 0);
+            if(a1 < 0)
+            {
+                a1 = 1;
+            }
+            return a1;
+        }
+    }
+}
