b54d32bda598661a6b63df53dfe311166ed73167
Hey-Mikelangelo <spikee.wave@gmail.com>
2022-03-12 18:32:40 +0100

    MathHelpers.GetGreatestCommonDivisor: Fix
    Fixed results with negative and zero numbers

--- lab_1/lab_1/MathHelpers.cs
+++ lab_1/lab_1/MathHelpers.cs
@@ -2,8 +2,16 @@
 {
     public static class MathHelpers
     {
+        /// <summary>
+        /// Calculates greatest common divisor using non recursive Euclid's algorithm
+        /// </summary>
+        /// <exception cref="System.DivideByZeroException"></exception>
         public static int GetGreatestCommonDivisor(int a, int b)
         {
+            if(a == 0 || b == 0)
+            {
+                throw new System.DivideByZeroException();
+            }
             if (a < b)
             {
                 int temp = a;
@@ -20,7 +28,7 @@
             while (b1 != 0);
             if(a1 < 0)
             {
-                a1 = 1;
+                a1 = -a1;
             }
             return a1;
         }
