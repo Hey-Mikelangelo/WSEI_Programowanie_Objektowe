b6eab77fb63a6fe6c1f38fa9c0a16272559cdb33
Hey-Mikelangelo <spikee.wave@gmail.com>
2022-03-19 20:40:21 +0100

    Person: class created

---
+++ lab_2/lab_2/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public class Person : IEquatable<Person>
+{
+    protected string name;
+    protected int age;
+
+    public string Name => name;
+    public int Age => age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public bool Equals([AllowNull] Person other)
+    {
+        if (this == other) return true;
+        return other != null && this.name == other.name && this.age == other.age;
+    }
+
+    public override string ToString()
+    {
+        return $"{name} ({age} y.o)";
+    }
+}
