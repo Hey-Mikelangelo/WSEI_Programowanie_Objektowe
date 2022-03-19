11a4862511287e40c9c43fc35ed79f69a8e038aa
Hey-Mikelangelo <spikee.wave@gmail.com>
2022-03-19 20:40:49 +0100

    Task: class created

---
+++ lab_2/lab_2/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public class Task : IEquatable<Task>{
+    private string name;
+    private TaskStatus status;
+
+    public string Name => name;
+    public TaskStatus Status { get => status; set => status = value; }
+
+    public Task(string name, TaskStatus status)
+    {
+        this.name = name;
+        this.status = status;
+    }
+
+    public override string ToString()
+    {
+        return $"{name} [{status}]";
+    }
+
+    public bool Equals([AllowNull] Task other)
+    {
+        if (this == other) return true;
+        return other != null && this.name == other.name && this.status == other.status;
+    }
+}
