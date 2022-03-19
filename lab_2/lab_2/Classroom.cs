81ce873a636efaf7229318216af5560d3b6f8656
Hey-Mikelangelo <spikee.wave@gmail.com>
2022-03-19 20:41:28 +0100

    Classroom: class created

---
+++ lab_2/lab_2/Classroom.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+public class Classroom
+{
+    private string name;
+    private Person[] persons;
+
+    public string Name => name;
+
+    public Classroom(string name, Person[] persons)
+    {
+        this.name = name;
+        this.persons = persons;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append($"Classroom: {name}\n\n");
+        for (int i = 0; i < persons.Length; i++)
+        {
+            stringBuilder.Append(persons[i].ToString());
+            stringBuilder.Append("\n\n");
+
+        }
+        return stringBuilder.ToString();
+    }
+}
